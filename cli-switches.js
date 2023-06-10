9c56e3b9d141b9fd762d6ddd9699c61d868b2921
by43673 <121001726+by43673@users.noreply.github.com>
2023-06-10 01:15:55 -0700

    Update cli-switches.js

--- cli-switches.js
+++ cli-switches.js
@@ -1,28 +1,11 @@
-"use strict";
-
-let yargs = require("yargs");
-
-/**
- * Append the correct CLI Switches to
- * the underlying Chrome instance
- *
- * @param {import("electron").App} app
- * @param {import("electron-store")} config
- */
-let cliSwitchHandler = function(app, config) {
-	const angleBackend = /** @type {string} */ (config.get("angleBackend", "default"));
-	const colorProfile = /** @type {string} */ (config.get("colorProfile", "default"));
-
-	app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required");
-
-	if (!config.get("acceleratedCanvas", true)) app.commandLine.appendSwitch("disable-accelerated-2d-canvas", "true");
-	if (config.get("disableFrameRateLimit", true)) {
 		app.commandLine.appendSwitch("disable-frame-rate-limit");
-		app.commandLine.appendSwitch("disable-oop-rasterization");
-		app.commandLine.appendSwitch("disable-oop-rasterization-ddl");
 		app.commandLine.appendSwitch("disable-pixel-canvas-recording");
-		app.commandLine.appendSwitch("disable-gpu-vsync");
-		app.commandLine.appendSwitch("disable-skia-renderer");
+		app.commandLine.appendSwitch("enable-gpu-rasterization");
+		app.commandLine.appendSwitch('disable-blink-features', 'CompositeSVG');
+		app.commandLine.appendSwitch("disable-oop-rasterization");
+		app.commandLine.appendSwitch('disable-2d-canvas-clip-aa');
+		app.commandLine.appendSwitch("disable-zero-copy");
+		app.commandLine.appendSwitch('disable-features', 'CanvasOopRasterization');
 		app.commandLine.appendSwitch('disable-features', 'ForcePreferredIntervalForVideo');
 		app.commandLine.appendSwitch('disable-features', 'UsePreferredIntervalForVideo');
 		app.commandLine.appendSwitch('disable-print-preview');
@@ -44,18 +27,5 @@ let cliSwitchHandler = function(app, config) {
 		app.commandLine.appendSwitch('enable-quic');
 		app.commandLine.appendSwitch('ignore-gpu-blocklist');
 		app.commandLine.appendSwitch('enable-highres-timer');
-		app.commandLine.appendSwitch("disable-features", "CanvasOopRasterization");
+		app.commandLine.appendSwitch("disable-gpu-vsync");
 		app.commandLine.appendSwitch('no-proxy-server');
-	}
-	if (config.get("inProcessGPU", false)) app.commandLine.appendSwitch("in-process-gpu");
-	if (angleBackend !== "default") app.commandLine.appendSwitch("use-angle", angleBackend);
-	if (colorProfile !== "default") app.commandLine.appendSwitch("force-color-profile", colorProfile);
-
-	yargs.parse(
-		/** @type {string} */
-		(config.get("chromiumFlags", "")),
-		(_, argv) => Object.entries(argv).slice(1, -1).forEach(entry => app.commandLine.appendSwitch(entry[0], entry[1]))
-	);
-};
-
-module.exports = cliSwitchHandler;
