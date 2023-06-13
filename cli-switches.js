53413f00d4cca7d934b0ca6c0ae920fe7b87bfe0
by43673 <121001726+by43673@users.noreply.github.com>
2023-06-13 00:55:12 -0700

    Update cli-switches.js

--- cli-switches.js
+++ cli-switches.js
@@ -1,18 +1,15 @@
-
-                app.commandLine.appendSwitch("disable-frame-rate-limit");
-		app.commandLine.appendSwitch("enable-gpu-rasterization");
+		app.commandLine.appendSwitch("disable-frame-rate-limit");
+		app.commandLine.appendSwitch('disable-layout-ng');
 		app.commandLine.appendSwitch('disable-features', 'CompositingOptimizations');
-		app.commandLine.appendSwitch("disable-oop-rasterization");
-		app.commandLine.appendSwitch("disable-zero-copy");
+		app.commandLine.appendSwitch("disable-gpu-rasterization");
 		app.commandLine.appendSwitch('disable-features', 'UsePreferredIntervalForVideo');
-		app.commandLine.appendSwitch('disable-print-preview');
+                app.commandLine.appendSwitch('disable-print-preview');
 		app.commandLine.appendSwitch('disable-metrics-repo');
 		app.commandLine.appendSwitch('disable-metrics');
 		app.commandLine.appendSwitch('disable-logging');
 		app.commandLine.appendSwitch('disable-hang-monitor');
 		app.commandLine.appendSwitch('disable-component-update');
 		app.commandLine.appendSwitch('enable-javascript-harmony');
-		app.commandLine.appendSwitch('enable-future-v8-vm-features');
 		app.commandLine.appendSwitch('disable-background-timer-throttling');
 		app.commandLine.appendSwitch('disable-renderer-backgrounding');
 		app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
@@ -22,8 +19,5 @@
 		app.commandLine.appendSwitch('high-dpi-support', '1');
 		app.commandLine.appendSwitch('no-pings');
 		app.commandLine.appendSwitch('enable-quic');
-		app.commandLine.appendSwitch('ignore-gpu-blocklist');
 		app.commandLine.appendSwitch('enable-highres-timer');
-		app.commandLine.appendSwitch("disable-gpu-vsync");
 		app.commandLine.appendSwitch('no-proxy-server');
-         
