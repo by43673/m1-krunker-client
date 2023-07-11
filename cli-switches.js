55f31dae73a393194870dd5b84c3ddd7645cee6a
by43673 <121001726+by43673@users.noreply.github.com>
2023-07-11 15:44:30 -0700

    Delete cli-switches.js

--- cli-switches.js
+++
@@ -1,3 +0,0 @@
-app.commandLine.appendSwitch('disable-frame-rate-limit');
-		app.commandLine.appendSwitch('disable-blink-features', 'LayoutNGFragmentItem');
-		app.commandLine.appendSwitch('disable-blink-features', 'LayoutNGFieldset');
