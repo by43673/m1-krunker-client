0c6b1a911ebc5f44332bf9db53a92f62294c5cad
by43673 <121001726+by43673@users.noreply.github.com>
2023-10-15 21:31:09 -0700

    Update switches.ts

--- src/switches.ts
+++ src/switches.ts
@@ -51,8 +51,9 @@ export function applyCommandLineSwitches(userPrefs: UserPrefs) {
 	if (userPrefs.fpsUncap) {
 		app.commandLine.appendSwitch('disable-frame-rate-limit');
 		app.commandLine.appendSwitch('disable-features', 'UsePreferredIntervalForVideo');
-		app.commandLine.appendSwitch('disable-blink-features', 'LayoutNGFieldset');
-		app.commandLine.appendSwitch('enable-blink-features', 'PaintHolding');
+		app.commandLine.appendSwitch('disable-features', 'LayoutNGFieldset');
+		app.commandLine.appendSwitch('disable-blink-features', 'LayoutNGFragmentItem');
+		app.commandLine.appendSwitch('disable-blink-features', 'EditingNG');
 	}
 
 	if (userPrefs['angle-backend'] !== 'default') {
