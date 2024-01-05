25010f73cc8b5f95b547092711a9fee09c962b6b
by43673 <121001726+by43673@users.noreply.github.com>
2024-01-05 00:25:35 -0800

    Update switches.ts

--- src/switches.ts
+++ src/switches.ts
@@ -4,88 +4,92 @@ import { app } from 'electron';
 
 /** applies command line switches to the app based on the passed userprefs */
 export function applyCommandLineSwitches(userPrefs: UserPrefs) {
-	if (userPrefs.safeFlags_removeUselessFeatures) {
-		app.commandLine.appendSwitch('disable-breakpad');
-		app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
-		app.commandLine.appendSwitch('disable-print-preview');
-		app.commandLine.appendSwitch('disable-2d-canvas-clip-aa');
-		app.commandLine.appendSwitch('disable-logging');
-		app.commandLine.appendSwitch('disable-hang-monitor');
-		app.commandLine.appendSwitch('disable-component-update');
-	}
-	if (userPrefs.safeFlags_helpfulFlags) {
-		app.commandLine.appendSwitch('disable-background-timer-throttling');
-		app.commandLine.appendSwitch('disable-renderer-backgrounding');
-	}
-	if (userPrefs.experimentalFlags_increaseLimits) {
-		app.commandLine.appendSwitch('enable-features', 'DefaultPassthroughCommandDecoder');
-	}
-	if (userPrefs.experimentalFlags_lowLatency) {
-		app.commandLine.appendSwitch('enable-highres-timer');
-		app.commandLine.appendSwitch('enable-quic');
-		app.commandLine.appendSwitch('ignore-gpu-blocklist');
-		app.commandLine.appendSwitch('high-dpi-support', '1');
-	}
-	if (userPrefs.experimentalFlags_experimental) {
-		app.commandLine.appendSwitch('enable-features', 'BlinkCompositorUseDisplayThreadPriority');
-	}
-	if (userPrefs.safeFlags_gpuRasterizing) {
-		app.commandLine.appendSwitch('enable-gpu-rasterization');
-		app.commandLine.appendSwitch('enable-oop-rasterization');
-		app.commandLine.appendSwitch('disable-zero-copy');
-	}
+    const switches: { [key: string]: string[] } = {
+        safeFlags_removeUselessFeatures: [
+            'disable-breakpad',
+            'autoplay-policy=no-user-gesture-required',
+            'disable-print-preview',
+            'disable-2d-canvas-clip-aa',
+            'disable-logging',
+            'disable-hang-monitor',
+            'disable-component-update'
+        ],
+        safeFlags_helpfulFlags: [
+            'disable-background-timer-throttling',
+            'disable-renderer-backgrounding'
+        ],
+        experimentalFlags_increaseLimits: ['enable-features=DefaultPassthroughCommandDecoder'],
+        experimentalFlags_lowLatency: [
+            'enable-highres-timer',
+            'enable-quic',
+            'ignore-gpu-blocklist',
+            'high-dpi-support=1'
+        ],
+        experimentalFlags_experimental: ['enable-features=BlinkCompositorUseDisplayThreadPriority'],
+        safeFlags_gpuRasterizing: [
+            'enable-gpu-rasterization',
+            'enable-oop-rasterization',
+            'disable-zero-copy'
+        ],
+        fpsUncap: [
+            'disable-frame-rate-limit',
+            'disable-gpu-vsync',
+            'disable-blink-features=ExperimentalIsInputPending',
+            'disable-blink-features=CompositeSVG',
+            'disable-blink-features=CompositeRelativeKeyframes',
+            'disable-blink-features=LayoutNGFragmentItem',
+            'disable-blink-features=LayoutNGFieldset',
+            'disable-blink-features=LayoutNGForControls',
+            'disable-blink-features=LayoutNGLayoutOverflow',
+            'disable-blink-features=LayoutNGWebkitBox',
+            'disable-blink-features=TableCellNewPercents',
+            'disable-features=UsePreferredIntervalForVideo',
+            'disable-features=ParentNodeReplaceChildren',
+            'disable-features=H264DecoderBufferIsCompleteFrame',
+            'disable-features=enable-pixel-canvas-recording',
+            'disable-features=FontPreloadingDelaysRendering',
+            'disable-features=AlignFontDisplayAutoTimeoutWithLCPGoal',
+            'disable-features=DecodeJpeg420ImagesToYUV',
+            'disable-blink-features=CSSFocusVisible',
+            'disable-blink-features=CSSFontMetricsOverride',
+            'disable-blink-features=CSSMarkerPseudoElement',
+            'disable-blink-features=CSSOMViewScrollCoordinates',
+            'disable-blink-features=CSSAspectRatioProperty',
+            'disable-blink-features=FlexAspectRatio',
+            'disable-blink-features=CSSKeyframesMemoryReduction',
+            'disable-blink-features=CSSPseudoIs',
+            'disable-blink-features=CSSPseudoWhere',
+            'disable-blink-features=CSSReducedFontLoadingLayoutInvalidations',
+            'disable-features=CompressParkableStrings',
+            'disable-features=ParkableStringsToDisk',
+            'disable-features=CompositeCrossOriginIframes',
+            'disable-features=VaapiVP9Encoder',
+            'disable-features=MediaCapabilitiesQueryGpuFactories'
+        ]
+    };
 
-	if (userPrefs.fpsUncap) {
-		app.commandLine.appendSwitch('disable-frame-rate-limit');
-		app.commandLine.appendSwitch('disable-gpu-vsync');
-		app.commandLine.appendSwitch('disable-blink-features', 'ExperimentalIsInputPending');	
-		app.commandLine.appendSwitch('disable-blink-features', 'CompositeSVG');
-		app.commandLine.appendSwitch('disable-blink-features', 'CompositeRelativeKeyframes');
-		app.commandLine.appendSwitch('disable-blink-features', 'LayoutNGFragmentItem');
-		app.commandLine.appendSwitch('disable-blink-features', 'LayoutNGFieldset');
-		app.commandLine.appendSwitch('disable-blink-features', 'LayoutNGForControls');
-		app.commandLine.appendSwitch('disable-blink-features', 'LayoutNGLayoutOverflow');
-		app.commandLine.appendSwitch('disable-blink-features', 'LayoutNGWebkitBox');
-		app.commandLine.appendSwitch('disable-blink-features', 'TableCellNewPercents');
-		app.commandLine.appendSwitch('disable-features', 'UsePreferredIntervalForVideo');
-		app.commandLine.appendSwitch('disable-features', 'ParentNodeReplaceChildren');
-		app.commandLine.appendSwitch('disable-features', 'H264DecoderBufferIsCompleteFrame');	
-		app.commandLine.appendSwitch('disable-features', 'FontPreloadingDelaysRendering');	
-		app.commandLine.appendSwitch('disable-features', 'AlignFontDisplayAutoTimeoutWithLCPGoal');
-		app.commandLine.appendSwitch('disable-features', 'DecodeJpeg420ImagesToYUV');	
-		app.commandLine.appendSwitch('disable-blink-features', 'CSSFocusVisible');
-		app.commandLine.appendSwitch('disable-blink-features', 'CSSFontMetricsOverride');
-		app.commandLine.appendSwitch('disable-blink-features', 'CSSMarkerPseudoElement');
-		app.commandLine.appendSwitch('disable-blink-features', 'CSSOMViewScrollCoordinates');
-		app.commandLine.appendSwitch('disable-blink-features', 'CSSAspectRatioProperty');
-		app.commandLine.appendSwitch('disable-blink-features', 'FlexAspectRatio');
-		app.commandLine.appendSwitch('disable-blink-features', 'CSSKeyframesMemoryReduction');
-		app.commandLine.appendSwitch('disable-blink-features', 'CSSPseudoIs');
-		app.commandLine.appendSwitch('disable-blink-features', 'CSSPseudoWhere');
-		app.commandLine.appendSwitch('disable-blink-features', 'CSSReducedFontLoadingLayoutInvalidations');
-		app.commandLine.appendSwitch('disable-features', 'CompressParkableStrings');
-		app.commandLine.appendSwitch('disable-features', 'ParkableStringsToDisk');
-		app.commandLine.appendSwitch('disable-features', 'CompositeCrossOriginIframes');
-		app.commandLine.appendSwitch('disable-features', 'VaapiVP9Encoder');
-		app.commandLine.appendSwitch('disable-features', 'MediaCapabilitiesQueryGpuFactories');		
-	}
+    for (const switchType in switches) {
+        if (userPrefs[switchType]) {
+            switches[switchType].forEach((switchItem) => {
+                app.commandLine.appendSwitch(switchItem);
+            });
+        }
+    }
 
-	if (userPrefs['angle-backend'] !== 'default') {
-		if (userPrefs['angle-backend'] === 'vulkan') {
-			app.commandLine.appendSwitch('use-angle', 'vulkan');
-			app.commandLine.appendSwitch('use-vulkan');
-			app.commandLine.appendSwitch('--enable-features=Vulkan');
+    if (userPrefs['angle-backend'] !== 'default') {
+        if (userPrefs['angle-backend'] === 'vulkan') {
+            app.commandLine.appendSwitch('use-angle', 'vulkan');
+            app.commandLine.appendSwitch('use-vulkan');
+            app.commandLine.appendSwitch('--enable-features=Vulkan');
+            console.log('VULKAN INITIALIZED');
+        } else {
+            app.commandLine.appendSwitch('use-angle', userPrefs['angle-backend'] as string);
+            console.log(`Using Angle: ${userPrefs['angle-backend']}`);
+        }
+    }
 
-			console.log('VULKAN INITIALIZED');
-		} else {
-			app.commandLine.appendSwitch('use-angle', userPrefs['angle-backend'] as string);
-
-			console.log(`Using Angle: ${userPrefs['angle-backend']}`);
-		}
-	}
-	if (userPrefs.inProcessGPU) {
-		app.commandLine.appendSwitch('in-process-gpu');
-		console.log('In Process GPU is active');
-	}
+    if (userPrefs.inProcessGPU) {
+        app.commandLine.appendSwitch('in-process-gpu');
+        console.log('In Process GPU is active');
+    }
 }
-
