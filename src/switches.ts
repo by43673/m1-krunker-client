82b0ac5ffe7ea5d1eb05ae9aa58999314032e082
by43673 <121001726+by43673@users.noreply.github.com>
2024-01-04 23:42:25 -0800

    Update switches.ts

--- src/switches.ts
+++ src/switches.ts
@@ -6,6 +6,7 @@ import { app } from 'electron';
 export function applyCommandLineSwitches(userPrefs: UserPrefs) {
 	if (userPrefs.safeFlags_removeUselessFeatures) {
 		app.commandLine.appendSwitch('disable-breakpad');
+		app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
 		app.commandLine.appendSwitch('disable-print-preview');
 		app.commandLine.appendSwitch('disable-2d-canvas-clip-aa');
 		app.commandLine.appendSwitch('disable-logging');
@@ -17,9 +18,7 @@ export function applyCommandLineSwitches(userPrefs: UserPrefs) {
 		app.commandLine.appendSwitch('disable-renderer-backgrounding');
 	}
 	if (userPrefs.experimentalFlags_increaseLimits) {
-		app.commandLine.appendSwitch('renderer-process-limit', '100');
-		app.commandLine.appendSwitch('max-active-webgl-contexts', '100');
-		app.commandLine.appendSwitch('webrtc-max-cpu-consumption-percentage', '100');
+		app.commandLine.appendSwitch('use-cmd-decoder', 'passthrough');
 	}
 	if (userPrefs.experimentalFlags_lowLatency) {
 		app.commandLine.appendSwitch('enable-highres-timer');
@@ -28,7 +27,7 @@ export function applyCommandLineSwitches(userPrefs: UserPrefs) {
 		app.commandLine.appendSwitch('high-dpi-support', '1');
 	}
 	if (userPrefs.experimentalFlags_experimental) {
-		app.commandLine.appendSwitch('use-cmd-decoder', 'passthrough');
+		app.commandLine.appendSwitch('enable-features', 'BlinkCompositorUseDisplayThreadPriority');
 	}
 	if (userPrefs.safeFlags_gpuRasterizing) {
 		app.commandLine.appendSwitch('enable-gpu-rasterization');
@@ -39,7 +38,6 @@ export function applyCommandLineSwitches(userPrefs: UserPrefs) {
 	if (userPrefs.fpsUncap) {
 		app.commandLine.appendSwitch('disable-frame-rate-limit');
 		app.commandLine.appendSwitch('disable-gpu-vsync');
-		app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
 		app.commandLine.appendSwitch('disable-blink-features', 'ExperimentalIsInputPending');	
 		app.commandLine.appendSwitch('disable-blink-features', 'CompositeSVG');
 		app.commandLine.appendSwitch('disable-blink-features', 'CompositeRelativeKeyframes');
@@ -48,9 +46,10 @@ export function applyCommandLineSwitches(userPrefs: UserPrefs) {
 		app.commandLine.appendSwitch('disable-blink-features', 'LayoutNGForControls');
 		app.commandLine.appendSwitch('disable-blink-features', 'LayoutNGLayoutOverflow');
 		app.commandLine.appendSwitch('disable-blink-features', 'LayoutNGWebkitBox');
+		app.commandLine.appendSwitch('disable-blink-features', 'TableCellNewPercents');
 		app.commandLine.appendSwitch('disable-features', 'UsePreferredIntervalForVideo');
 		app.commandLine.appendSwitch('disable-features', 'ParentNodeReplaceChildren');
-		app.commandLine.appendSwitch('disable-features', 'enable-pixel-canvas-recording');	
+		app.commandLine.appendSwitch('disable-features', 'H264DecoderBufferIsCompleteFrame');	
 		app.commandLine.appendSwitch('disable-features', 'FontPreloadingDelaysRendering');	
 		app.commandLine.appendSwitch('disable-features', 'AlignFontDisplayAutoTimeoutWithLCPGoal');
 		app.commandLine.appendSwitch('disable-features', 'DecodeJpeg420ImagesToYUV');	
@@ -59,17 +58,16 @@ export function applyCommandLineSwitches(userPrefs: UserPrefs) {
 		app.commandLine.appendSwitch('disable-blink-features', 'CSSMarkerPseudoElement');
 		app.commandLine.appendSwitch('disable-blink-features', 'CSSOMViewScrollCoordinates');
 		app.commandLine.appendSwitch('disable-blink-features', 'CSSAspectRatioProperty');
+		app.commandLine.appendSwitch('disable-blink-features', 'FlexAspectRatio');
 		app.commandLine.appendSwitch('disable-blink-features', 'CSSKeyframesMemoryReduction');
 		app.commandLine.appendSwitch('disable-blink-features', 'CSSPseudoIs');
 		app.commandLine.appendSwitch('disable-blink-features', 'CSSPseudoWhere');
 		app.commandLine.appendSwitch('disable-blink-features', 'CSSReducedFontLoadingLayoutInvalidations');
-		app.commandLine.appendSwitch('disable-blink-features', 'CSSReducedFontLoadingInvalidations');
-		app.commandLine.appendSwitch('disable-features', 'VaapiVP9Encoder');
-		app.commandLine.appendSwitch('disable-features', 'MediaCapabilitiesQueryGpuFactories');	
 		app.commandLine.appendSwitch('disable-features', 'CompressParkableStrings');
 		app.commandLine.appendSwitch('disable-features', 'ParkableStringsToDisk');
 		app.commandLine.appendSwitch('disable-features', 'CompositeCrossOriginIframes');
-
+		app.commandLine.appendSwitch('disable-features', 'VaapiVP9Encoder');
+		app.commandLine.appendSwitch('disable-features', 'MediaCapabilitiesQueryGpuFactories');		
 	}
 
 	if (userPrefs['angle-backend'] !== 'default') {
