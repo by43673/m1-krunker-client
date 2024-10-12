44be8f9fd934c0a47f1d4fea1b7a02ec40d0e863
by43673 <121001726+by43673@users.noreply.github.com>
2024-10-12 02:19:23 -0700

    Update switches.ts

--- src/switches.ts
+++ src/switches.ts
@@ -37,12 +37,12 @@ export function applyCommandLineSwitches(userPrefs: UserPrefs) {
 		console.log('Applied latency-reducing flags');
 	}
 	if (userPrefs.experimentalFlags_experimental) {
-		app.commandLine.appendSwitch('enable-features', 'DefaultPassthroughCommandDecoder');
-		app.commandLine.appendSwitch('enable-features', 'BlinkCompositorUseDisplayThreadPriority');
-		app.commandLine.appendSwitch('enable-features', 'GpuUseDisplayThreadPriority');
-		app.commandLine.appendSwitch('enable-features', 'GpuProcessHighPriorityWin'); 	
+		app.commandLine.appendSwitch('use-cmd-decoder', 'passthrough');
+		app.commandLine.appendSwitch('enable-passthrough-raster-decoder');
 		app.commandLine.appendSwitch('use-angle', 'gl');	
 		app.commandLine.appendSwitch('use-gl', 'angle');
+		app.commandLine.appendSwitch('enable-features', 'BlinkCompositorUseDisplayThreadPriority');
+		app.commandLine.appendSwitch('enable-features', 'GpuUseDisplayThreadPriority');		
 		console.log('Enabled Experiments');
 	}
 	if (userPrefs.safeFlags_gpuRasterizing) {
